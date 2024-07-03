413ad35c4aecc0510772f78f1fde8fcde9f6808b
Víctor Ávila <victoraavila2000@gmail.com>
2024-07-03 20:16:31 -0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Víctor Ávila on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
