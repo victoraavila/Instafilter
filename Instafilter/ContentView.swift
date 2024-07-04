d222a5e7f1c83f150cc0524f34d5b1b557806c3a
Víctor Ávila <victoraavila2000@gmail.com>
2024-07-04 00:14:13 -0300

    Basic image filtering using Core Image

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,6 +5,8 @@
 //  Created by Víctor Ávila on 03/07/24.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import PhotosUI
 import SwiftUI
 
@@ -21,6 +23,12 @@ struct ContentView: View {
     // 3. Place a PhotosPickerView whenever we want to trigger photo selection (we will wrap the if let-else and use it as a label)
     // 4. Have a method that will be called when an Image is selected: we're gonna load a binary blob of data from PhotosPickerItem? and feed that into UIImage.
     @State private var selectedItem: PhotosPickerItem?
+    
+    // A context, in CoreImage, is an object responsible for rendering a CIImage to a CGImage. In practice, it converts the recipe of an image to an actual series of pixels we can work with.
+    // Contexts are expensive to create: if you will render many images, you should create a context once (when you app starts up) and keep it alive
+    // The filter will be marked with @State because we want it to be flexible (so the user can change from Sepia to another).
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     var body: some View {
         NavigationStack {
@@ -51,6 +59,10 @@ struct ContentView: View {
                     // Our intensity slider
                     Text("Intensity")
                     Slider(value: $filterIntensity)
+                    // Changing the slider won't automatically trigger our applyProcessing() method
+                    // We have to tell SwiftUI to watch filter intensity with .onChange(). This can go anywhere in the View hierarchy
+                        .onChange(of: filterIntensity, applyProcessing)
+                    // It appears to be slow on the simulator, but it is lightning fast on an iPhone
                 }
                 
                 HStack {
@@ -78,10 +90,26 @@ struct ContentView: View {
             guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return } // If it fails, return
             guard let inputImage = UIImage(data: imageData) else { return } // If it fails, return
             
-            // more code to come
+            // It needs to send whatever picture was chosen into the Sepia Tone filter, then call applyProcessing()
+            // CoreImage filters have a dedicated inputImage property that let us send in a CIImage for the filter to work with, but this is often bizarrely broken. It is much safer to use a filter's .setValue() with the KCIInputImage key.
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
             
             // This will run whenever selectedItem changes (we will do this by attaching the .onChange())
         }
+    }
+    
+    // This method will process whatever image the user imported: it will set the Sepia Tone intensity to the filterIntensity read from the slider, then run the transformation, then read the output image back from the filter, ask our CIContext to render it, then place the result into processedImage.
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity) // The Sepia Tone only accepts Floats, no Doubles
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return } // Don't read part of it, read all of it
+        
+        // At this point, we have actual pixels in cgImage
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
     }
 }
 
